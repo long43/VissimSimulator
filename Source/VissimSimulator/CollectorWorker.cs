7f8ed1016bb3fbbbbff60639daad9638a4c8480f
Yi Zhang (BING MAP) <yzh@microsoft.com>
2016-12-21 12:55:12 -0800

    Changes made

--- Source/VissimSimulator/CollectorWorker.cs
+++ Source/VissimSimulator/CollectorWorker.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Oracle.DataAccess.Client;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
 
 namespace VissimSimulator
 {
@@ -18,32 +15,11 @@ namespace VissimSimulator
         /// <param name="evt"></param>
         public void Process(CellularTowerEvent evt)
         {
-            //at least we need to persisit the CellularTowerEvent
-            //read the data into database;
-            using (OracleConnection con = new OracleConnection(
-                ///TODO add the right connection path for ORACLE database
-                ))
-            {
-                con.ConnectionString = "host = serverName;databse = myDatabse; uid = userName; pwd = password";
-                con.Open();
-            }
-                while (true)
-            {
-                try
-                {
-                    int locationId = int.Parse(evt.LocationId);
-                    int cellularTowerId = int.Parse(evt.CellularTowerId);
-                    string eventType = Convert.ToString(evt.Event.EventType);
-                    //TODO Which time we should use?
-                    //Is the data format correct? Does C# have the data type like Timestamp for ORACLE?
-                    string eventTimeSpan = (evt.CurrentTick).ToString();
-                    AddEvent(locationId, cellularTowerId, eventType, eventTimeSpan);
-                }
-                catch
-                {
-                    Console.WriteLine("Input error");
-                }
-            }
+            int locationId = int.Parse(evt.LocationId);
+            int cellularTowerId = int.Parse(evt.CellularTowerId);
+            string eventType = Convert.ToString(evt.Event.EventType);
+            long eventTimeSpan = evt.CurrentTick;
+            AddEvent(locationId, cellularTowerId, eventType, eventTimeSpan);
         }
         /// <summary>
         /// Insert output data into the SQL database table.
@@ -51,14 +27,12 @@ namespace VissimSimulator
         /// <param name="locatioanId">The location id of the cell station.</param>
         /// <param name="cellularTowerId">The cell id of the cell station.</param>
         /// <param name="eventType">The type of the event.</param>
-        /// <param name="eventTimeSpan">The time of the event when it occurs.</param>
-        static void AddEvent(int locationId, int cellularTowerId, string eventType, string eventTimeSpan)
+        /// <param name="eventTimestamp">The time of the event when it occurs.</param>
+        static void AddEvent(int locationId, int cellularTowerId, string eventType, long eventTimeTick)
         {
-            using (OracleConnection con = new OracleConnection(
-                ///TODO add the right connection path for ORACLE database
-                ))
+            using (OracleConnection con = new OracleConnection())
             {
-                con.ConnectionString = "host = serverName;databse = myDatabse; uid = userName; pwd = password";
+                con.ConnectionString = ConfigurationManager.AppSettings["SqlConnectionString"];
                 con.Open();
                 try
                 {
@@ -68,7 +42,7 @@ namespace VissimSimulator
                         command.Parameters.Add(new OracleParameter("LocationId", locationId));
                         command.Parameters.Add(new OracleParameter("CellularTowerId", cellularTowerId));
                         command.Parameters.Add(new OracleParameter("EventType", eventType));
-                        command.Parameters.Add(new OracleParameter("EventTimeSpan", eventTimeSpan));
+                        command.Parameters.Add(new OracleParameter("EventTimeSpan", eventTimeTick));
                         command.ExecuteNonQuery();
                     }
                 }
